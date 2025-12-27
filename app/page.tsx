f26c1ca76d5091a1168c860d4e2ec3b51393c890
mehediemon007 <mehediemon8=7@gmail.com>
2025-12-27 21:18:47 +0600

    sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="text-7xl font-bold font-sync">I&apos;m Alex Walker Digital designer and illustrator.</h1>
+            
         </>
     );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FaFacebook, FaLinkedin, FaGithub, FaEnvelope, FaXTwitter } from "react-icons/fa6";
 
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className='sidebar-container'>
+            <div className="profile-section">
+                <div>
+                    <p className='text-[1.8rem] font-bold font-sync text-gray-100'>Mehedi Hasan Emon</p>
+                    <Image src="/image.webp" alt="Emon" width={240} height={240} />
+                </div>
+                <div className='text-gray-100 font-sync'>
+                    <p className='text-[13px]'>Specialization:</p>
+                    <h6 className='text-base font-bold'>Frontend Developer</h6>
+                </div>
+                <div className='text-gray-100 font-sync'>
+                    <p className='text-[13px]'>Based in:</p>
+                    <h6 className='text-base font-bold'>Dhaka, Bangladesh</h6>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <a href="#" className='inline-flex justify-center items-center w-11 h-11 text-gray-100 bg-[#161616] border border-[#4b4b51] rounded-[20px]'><FaGithub/></a>
+                    <a href="#" className='inline-flex justify-center items-center w-11 h-11 text-gray-100 bg-[#161616] border border-[#4b4b51] rounded-[20px]'><FaLinkedin/></a>
+                    <a href="#" className='inline-flex justify-center items-center w-11 h-11 text-gray-100 bg-[#161616] border border-[#4b4b51] rounded-[20px]'><FaFacebook/></a>
+                    <a href="#" className='inline-flex justify-center items-center w-11 h-11 text-gray-100 bg-[#161616] border border-[#4b4b51] rounded-[20px]'><FaXTwitter/></a>
+                    <a href="mailto:mehediemon7@gmail.com" className='inline-flex justify-center items-center w-11 h-11 text-gray-100 bg-[#161616] border border-[#4b4b51] rounded-[20px]'><FaEnvelope/></a>
+                </div>
+                <Link href='#contact'>Let&apos;s Work Together!</Link>
+            </div>
         </div>
     )
 }
